a5c31e8d201a94f52f3e0fa2457977d540cda91e
Markus Zellner <markus@zellner-mail.de>
2020-11-19 20:26:52 +0100

    Cleanup

--- src/main/java/de/alamos/fe2/external/impl/Parameter.java
+++ src/main/java/de/alamos/fe2/external/impl/Parameter.java
@@ -2,26 +2,18 @@ package de.alamos.fe2.external.impl;
 
 import de.alamos.fe2.external.enums.EAlarmDataEntries;
 
+/**
+ * This is a subset of the official Alamos Enums.
+ * Intention is that we don't need everything and that we want to 
+ * extend by custom parameters.
+ */
 public enum Parameter {
     //First the official ones copied from class
-    TEXT(EAlarmDataEntries.TEXT.getKey()),
     STREET(EAlarmDataEntries.STREET.getKey()),
     HOUSE(EAlarmDataEntries.HOUSE.getKey()),
     CITY(EAlarmDataEntries.CITY.getKey()),
     POSTALCODE(EAlarmDataEntries.POSTALCODE.getKey()),
-    BUILDING_NAME(EAlarmDataEntries.BUILDING_NAME.getKey()),
-    LOCATION_ADDITIOnAL(EAlarmDataEntries.LOCATION_ADDITIOnAL.getKey()),
-    KEYWORD(EAlarmDataEntries.KEYWORD.getKey()),
-    KEYWORD_DESCRIPTION(EAlarmDataEntries.KEYWORD_DESCRIPTION.getKey()),
-    KEYWORD_IDENTIFICATION(EAlarmDataEntries.KEYWORD_IDENTIFICATION.getKey()),
-    KEYWORD_ADDITIONAL(EAlarmDataEntries.KEYWORD_ADDITIONAL.getKey()),
-    KEYWORD_CATEGORY(EAlarmDataEntries.KEYWORD_CATEGORY.getKey()),
-    CALLER(EAlarmDataEntries.CALLER.getKey()),
-    CALLER_CONTACT(EAlarmDataEntries.CALLER_CONTACT.getKey()),
-    LAT(EAlarmDataEntries.LAT.getKey()),
-    LNG(EAlarmDataEntries.LNG.getKey()),
-    DESTINATION(EAlarmDataEntries.DESTINATION.getKey()),
-    VEHICLES("vehicles"), // should be part of enum
+    VEHICLES("vehicles"), // should be part of official enum
 
     //Now custom ones
     BEMERKUNG("custom_bemerkung"),
